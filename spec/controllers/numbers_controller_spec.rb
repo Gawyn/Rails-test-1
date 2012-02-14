e1f2818b77730994cfa48a8bf74b06b572b25396
Gawyn <cplanas.work@gmail.com>
2012-02-14 12:57:12 +0200

    Rspec tests modified

--- spec/controllers/numbers_controller_spec.rb
+++ spec/controllers/numbers_controller_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 describe NumbersController do
-  describe 'GET new' do
+  describe 'new' do
     #As this action only shows its view, we don't have anything to test.
   end
-  describe 'POST create' do
+  describe 'create' do
     it 'returns a page with the number in words' do
       n = Random.rand(100000000)
-      post 'create', {:number => n}
-      page.should have_content(n.in_words)
+      get 'create', {:number => n}
+      assigns[:number].should == n.in_words
     end
     it 'redirects to new' do
-      post 'create', {:number => 1234}
-      response.should redirect_to root_path
+      get 'create', {:number => 1234}
+      response.should render_template 'new'
     end
   end
 end
